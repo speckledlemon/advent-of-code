8e2eae344e396b8991aeabea58f99de139b376b3
speckledlemon <speckledlemon@local>
2019-12-02 23:25:16 -0500

    clean up types

--- day2.nim
+++ day2.nim
@@ -1,18 +1,16 @@
 from sequtils import map
-from strutils import split, parseUInt
+from strutils import split, parseBiggestUInt
 import strutils
 from sugar import `=>`
 import ./aoc_utils
 
-proc processProgram(state: seq[uint]): seq[uint] =
+proc processProgram[T: SomeInteger](state: seq[T]): seq[T] =
   # make a mutable copy of the input
-  result = newSeq[uint]()
+  result = newSeq[T]()
   for i in low(state)..high(state):
     result.add(state[i])
-  # indices for selecting the tape window
-  var
-    startIdx: uint = 0
-  while (startIdx + 4) <= uint(high(result)):
+  var startIdx: T = 0
+  while (startIdx + 4) <= T(high(result)):
     let
       opcode = result[startIdx]
     if opcode == 1:
@@ -26,25 +24,22 @@ proc processProgram(state: seq[uint]): seq[uint] =
     else:
       raise
 
-
 when isMainModule:
-  doAssert processProgram(@[1, 0, 0, 0, 99].map(v => uint(v))) == @[2, 0, 0, 0, 99].map(v => uint(v))
-  doAssert processProgram(@[2, 3, 0, 3, 99].map(v => uint(v))) == @[2, 3, 0, 6, 99].map(v => uint(v))
-  doAssert processProgram(@[2, 4, 4, 5, 99, 0].map(v => uint(v))) == @[2, 4, 4, 5, 99, 9801].map(v => uint(v))
-  doAssert processProgram(@[1, 1, 1, 4, 99, 5, 6, 0, 99].map(v => uint(v))) == @[30, 1, 1, 4, 2, 5, 6, 0, 99].map(v => uint(v))
-  doAssert processProgram(@[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50].map(v => uint(v))) == @[3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50].map(v => uint(v))
-  var unprocessedProgram = readAllLines("day2_input.txt")[0].split(',').map(i => parseUInt(i))
+  doAssert processProgram(@[1, 0, 0, 0, 99]) == @[2, 0, 0, 0, 99]
+  doAssert processProgram(@[2, 3, 0, 3, 99]) == @[2, 3, 0, 6, 99]
+  doAssert processProgram(@[2, 4, 4, 5, 99, 0]) == @[2, 4, 4, 5, 99, 9801]
+  doAssert processProgram(@[1, 1, 1, 4, 99, 5, 6, 0, 99]) == @[30, 1, 1, 4, 2, 5, 6, 0, 99]
+  doAssert processProgram(@[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]) == @[3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+  var unprocessedProgram = readAllLines("day2_input.txt")[0].split(',').map(i => parseBiggestUInt(i))
   unprocessedProgram[1] = 12
   unprocessedProgram[2] = 2
-  echo unprocessedProgram
   var output = processProgram(unprocessedProgram)[0]
   echo "part 1: ", output
 
   for noun in 0..100:
     for verb in 0..100:
-      unprocessedProgram[1] = uint(noun)
-      unprocessedProgram[2] = uint(verb)
+      unprocessedProgram[1] = BiggestUInt(noun)
+      unprocessedProgram[2] = BiggestUInt(verb)
       output = processProgram(unprocessedProgram)[0]
       if output == 19690720:
         echo "part 2: ", 100 * noun + verb
-        break
